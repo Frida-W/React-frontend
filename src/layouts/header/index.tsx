9a95a48d02190fbf683e3328926ab76645cad7c9
Frida <frida.wang@outlook.com>
2024-06-20 23:35:09 +1000

    fix

--- src/layouts/header/index.tsx
+++ src/layouts/header/index.tsx
@@ -36,28 +36,24 @@ const index = () => {
   return (
     <div className="relative flex items-center w-full py-6 px-3 lg:px-4 transition-all gap-3 sm:gap-4">
       {/* <Logo></Logo> */}
-
       <div
         className="absolute w-56 h-28 top-0 left-0 pointer-events-none"
         style={{
           background: `left/cover no-repeat url(${xmaxHeaderBg})`,
         }}
       />
-
       <div className="hidden lg:flex lg:w-auto lg:space-x-5">
         {tagValues.map((tag) => (
           <Tag key={tag.to} icon={tag.icon} to={tag.to} title={tag.title}></Tag>
         ))}
       </div>
-
+      s
       <div className="flex ml-auto">
         <Login />
       </div>
-
       <div className="hidden lg:block">
         <LanguageBar />
       </div>
-
       <div className="flex items-center gap-3">
         <div className="hidden lg:block">
           <IconButton to="">
